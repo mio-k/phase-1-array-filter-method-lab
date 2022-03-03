eb06cd37ec0d96a119e7117d4f5d6fdaa21f0539
mio kataoka <mio.kanna@gmail.com>
2022-03-02 20:40:15 -0800

    checking in the problem code

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+// function findMatching(drivers, str){
+//     let item = item.toUpperCase;
+//     for (item of drivers){
+//       if(item === str){
+
+//       }
+//     }
+// }
+// function findMatching(drivers, str){
+//     const upperCased = [];
+//     for (let driver of drivers){
+//         upperCased.push(driver.toUpperCase());
+//     }
+//     str = str.toUpperCase();
+//     const result = drivers.filter(driver => driver === str);
+//     return result;
+// }
+function findMatching(drivers, str){
+    const upperCased = [];
+    for (let driver of drivers){
+        upperCased.push(driver.toUpperCase());
+    }
+    str = str.toUpperCase();
+    const result = upperCased.filter(driver => driver === str);
+    return result;
+}
